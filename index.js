29a69dcc4d61d8b7f407a834dbac25f814e011cc
Marycharity <marycharity5@gmail.com>
2025-04-03 03:24:29 +0300

    Add README

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Add your code here
+function submitData(name, email) {
+    const userData = { name, email };
+
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify(userData),
+    })
+    .then(response => {
+        return response.json();
+    })
+    .then(data => {
+        document.body.innerHTML += `<p>${data.id}</p>`;  
+        return data;
+    })
+    .catch(error => {
+        document.body.innerHTML += `<p>${error.message}</p>`;  
+    });
+}
